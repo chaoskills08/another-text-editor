dcf3d1844f13b3300f383f40a4045cdb6f57f724
Nicholas Bales <nickb96@icloud.com>
2022-09-29 02:36:26 -0400

    added functional preliminary code to js files

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -15,34 +15,35 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-        new HtmlWebpackPlugin({
-          template: './index.html',
-          title: "JATE"
-        }),
-        new InjectManifest({
-          swSrc: './src-sw.js',
-          swDest: "src-sw.js"
-        }),
-        new WebpackPwaManifest({
-          fingerprints: false,
-          inject: true,
-          name: 'Just Another Text Editor',
-          short_name: 'JATE',
-          description: 'Just another text editor',
-          background_color: '#f5f5f5',
-          theme_color: '#225ca3',
-          start_url: '/',
-          publicPath: '/',
-          crossorigin: 'anonymous',
-          icons: [
-            {
-              src: path.resolve('src/images/logo.png'),
-              sizes: [96, 128, 192, 256, 384, 512],
-              destination: path.join('assets', 'icons')
-            }
-          ]
-        })
-      ],
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'J.A.T.E.'
+      }),
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E.',
+        description: 'Javascript text editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+    ],
 
     module: {
       rules: [
@@ -58,8 +59,8 @@ module.exports = () => {
             options: {
               presets: ['@babel/preset-env'],
               plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
-            }
-          }
+            },
+          },
         }
       ],
     },
